1cbc083b91559d48dbd052ca799734072e6a7df8
Jurģis Brigmanis <velko.x@gmail.com>
2023-12-04 22:29:09 +0200

    Part one

--- 02/cubes.c
+++ 02/cubes.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 
 
 #define BUFFER_SIZE     256
 
-static int parse_line(const char *line);
+static int parse_line(char *line);
 
 int main(void)
 {
@@ -16,6 +17,7 @@ int main(void)
     {
         if (fgets(line, BUFFER_SIZE, input) == NULL) break; 
         total += parse_line(line);
+        //break;
     }
 
     printf("Result: %d\n", total);
@@ -24,8 +26,45 @@ int main(void)
     return 0;
 }
 
-
-static int parse_line(const char *line)
+static int parse_line(char *line)
 {
+    char *outer_savep, *inner_savep;
+    char *game_token = strtok_r(line, ":", &outer_savep);
+
+    int game_id;
+    sscanf(game_token, "%*s %d", &game_id);
+
+    int max_red = 0, max_green = 0, max_blue = 0;
+
+    while (game_token = strtok_r(NULL, ";", &outer_savep))
+    {
+        char *pull_token = strtok_r(game_token, ",", &inner_savep);
+        while (pull_token)
+        {
+            int count; char color;
+            sscanf(pull_token, "%d %c", &count, &color);
+
+            switch (color)
+            {
+            case 'r':
+                if (max_red < count) max_red = count;
+                break;
+            case 'g':
+                if (max_green < count) max_green = count;
+                break;
+            case 'b':
+                if (max_blue < count) max_blue = count;
+                break;
+            }
+            pull_token = strtok_r(NULL, ",", &inner_savep);
+        }
+    }
+
+    if (max_red <= 12 && max_green <= 13 && max_blue <= 14)
+        return game_id;
+
+    printf("%d\n", game_id);
+    printf("red: %d, green: %d, blue: %d\n", max_red, max_green, max_blue);
+
     return 0;
 }
