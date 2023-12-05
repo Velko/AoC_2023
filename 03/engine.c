2fb7f4ebcd644c730ee28044a7c851416f7f6e26
Jurģis Brigmanis <velko.x@gmail.com>
2023-12-05 21:35:33 +0200

    Number search / parse

--- 03/engine.c
+++ 03/engine.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 
 #define BUFFER_SIZE     150 /* input appears to be 140x140, but let's add a bit extra for \n and \0 */
 
 char lines[BUFFER_SIZE][BUFFER_SIZE];
 
-static int parse_line(const char *line);
+static int parse_num(int col, int row);
 
 int main(void)
 {
@@ -19,6 +20,8 @@ int main(void)
         if (fgets(lines[num_rows], BUFFER_SIZE, input) == NULL) break; 
     }
 
+    num_rows = 3;
+
     for (int row = 0; row < num_rows; ++row)
     {
         for (const char *c = lines[row]; *c; ++c)
@@ -26,7 +29,9 @@ int main(void)
             if (*c != '.' && *c != '\n' && !isdigit(*c))
             {
                 int col = c - lines[row];
-                printf("(%d, %d)\t%c\n", col, row, *c);
+                printf("%c  ", *c);
+                int num = parse_num(col + 1, row - 1);
+                printf("%d\n", num);
             }
         }
     }
@@ -36,7 +41,28 @@ int main(void)
 }
 
 
-static int parse_line(const char *line)
+static int parse_num(int col, int row)
 {
+    char digits[16];
+
+    char *p = lines[row] + col;
+
+    if (!isdigit(*p)) return 0;
+
+    char *sp = p;
+    for (; isdigit(*sp) && sp >= lines[row]; --sp);
+
+    char *ep = p;
+    for (; ep && isdigit(*ep); ++ep);
+
+    int len = ep - sp - 1;
+
+
+    strncpy(digits, sp + 1, len);
+    digits[len] = 0;
+
+
+    printf("/%s/ ", digits);
+
     return 0;
 }
