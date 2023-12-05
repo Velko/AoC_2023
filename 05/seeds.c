175a6a9ca5113fecb59999c39c7774c53d6bbed2
Jurģis Brigmanis <velko.x@gmail.com>
2023-12-06 00:07:29 +0200

    Parse maps

--- 05/seeds.c
+++ 05/seeds.c
@@ -1,31 +1,74 @@
 #include <stdio.h>
-
+#include <string.h>
 
 #define BUFFER_SIZE     256
+#define MAX_RANGES      200  /* there are ~220 lines total in input, should be enough */
 
-static int parse_line(const char *line);
+struct map_range
+{
+    long dest;
+    long src;
+    long limit;
+};
+
+struct map_range seed_to_soil[MAX_RANGES];
+struct map_range soil_to_fertilizer[MAX_RANGES];
+struct map_range fertilizer_to_water[MAX_RANGES];
+struct map_range water_to_light[MAX_RANGES];
+struct map_range light_to_temperature[MAX_RANGES];
+struct map_range temperature_to_humidity[MAX_RANGES];
+struct map_range humidity_to_location[MAX_RANGES];
+
+static void parse_seeds(char *seeds);
+static void read_map(FILE *input, struct map_range *range);
 
 int main(void)
 {
     char line[BUFFER_SIZE];
     FILE *input = fopen("input.txt", "r");
 
-    int total = 0;
-
     for (;;)
     {
         if (fgets(line, BUFFER_SIZE, input) == NULL) break; 
-        total += parse_line(line);
-    }
 
-    printf("Result: %d\n", total);
+        if (strstr(line, "seeds: ") == line)
+            parse_seeds(line + 7);
+        else if (strstr(line, "seed-to-soil map:") == line)
+            read_map(input, seed_to_soil);
+        else if (strstr(line, "soil-to-fertilizer map:") == line)
+            read_map(input, soil_to_fertilizer);
+        else if (strstr(line, "fertilizer-to-water map:") == line)
+            read_map(input, fertilizer_to_water);
+        else if (strstr(line, "water-to-light map:") == line)
+            read_map(input, water_to_light);
+        else if (strstr(line, "light-to-temperature map:") == line)
+            read_map(input, light_to_temperature);
+        else if (strstr(line, "temperature-to-humidity map:") == line)
+            read_map(input, temperature_to_humidity);
+        else if (strstr(line, "humidity-to-location map:") == line)
+            read_map(input, humidity_to_location);
+    }
 
     fclose(input);
     return 0;
 }
 
-
-static int parse_line(const char *line)
+static void parse_seeds(char *seeds)
 {
-    return 0;
+    printf("S: %s", seeds);
+}
+
+static void read_map(FILE *input, struct map_range *range)
+{
+    int count = 0;
+    long length;
+
+    for (;;++count) {
+        int nread = fscanf(input, "%ld %ld %ld", &range[count].dest, &range[count].src, &length);
+        if (nread != 3) break;
+        range[count].limit = range[count].src + length;
+
+        printf("[%ld:%ld) -> %ld\n", range[count].src, range[count].limit, range[count].dest);
+    }
+    range[count].limit = 0;
 }
