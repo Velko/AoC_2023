b54834fe02af3e6a7e03beb1391b73a05c98b94c
Jurģis Brigmanis <velko.x@gmail.com>
2023-12-05 21:18:23 +0200

    Load input and scan symbols

--- 03/engine.c
+++ 03/engine.c
@@ -1,24 +1,35 @@
 #include <stdio.h>
+#include <ctype.h>
 
 
-#define BUFFER_SIZE     256
+#define BUFFER_SIZE     150 /* input appears to be 140x140, but let's add a bit extra for \n and \0 */
+
+char lines[BUFFER_SIZE][BUFFER_SIZE];
 
 static int parse_line(const char *line);
 
 int main(void)
 {
-    char line[BUFFER_SIZE];
     FILE *input = fopen("input.txt", "r");
 
-    int total = 0;
+    int num_rows = 0;
 
-    for (;;)
+    for (;; ++num_rows)
     {
-        if (fgets(line, BUFFER_SIZE, input) == NULL) break; 
-        total += parse_line(line);
+        if (fgets(lines[num_rows], BUFFER_SIZE, input) == NULL) break; 
     }
 
-    printf("Result: %d\n", total);
+    for (int row = 0; row < num_rows; ++row)
+    {
+        for (const char *c = lines[row]; *c; ++c)
+        {
+            if (*c != '.' && *c != '\n' && !isdigit(*c))
+            {
+                int col = c - lines[row];
+                printf("(%d, %d)\t%c\n", col, row, *c);
+            }
+        }
+    }
 
     fclose(input);
     return 0;
