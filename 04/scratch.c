55cc89a79049af70a15e0e6a612881e00183dbd7
Jurģis Brigmanis <velko.x@gmail.com>
2023-12-05 22:36:41 +0200

    Parsing

--- 04/scratch.c
+++ 04/scratch.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 
 #define BUFFER_SIZE     256
+#define N_WINNING        10
+#define N_GUESSES        25
 
-static int parse_line(const char *line);
+
+static int parse_line(char *line);
 
 int main(void)
 {
@@ -25,7 +30,43 @@ int main(void)
 }
 
 
-static int parse_line(const char *line)
+static void parse_numbers(char *row, int *results);
+
+static int parse_line(char *line)
 {
+    char *outer_savep;
+    char *card_token = strtok_r(line, ":", &outer_savep);
+
+
+    int card_id;
+    sscanf(card_token, "%*s %d", &card_id);
+
+    printf("C %d\n", card_id);
+
+    card_token = strtok_r(NULL, "|", &outer_savep);
+
+    int winning_numbers[N_WINNING];
+    parse_numbers(card_token, winning_numbers);
+
+
+    card_token = strtok_r(NULL, "|", &outer_savep);
+    int guessed_numbers[N_GUESSES];
+    parse_numbers(card_token, guessed_numbers);
+
     return 0;
+}
+
+static void parse_numbers(char *row, int *results)
+{
+    char *inner_savep;
+    int count = 0;
+
+    char *num_token = strtok_r(row, " ", &inner_savep);
+    while (num_token)
+    {
+        results[count++] = atoi(num_token);
+        num_token = strtok_r(NULL, " ", &inner_savep);
+    }
+
+    printf ("%d\n", count);
 }
