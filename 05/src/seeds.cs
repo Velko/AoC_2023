c798ff67879852325f48d1b10fa1a9a0cc4e5ce4
Jurģis Brigmanis <velko.x@gmail.com>
2023-12-06 22:43:35 +0200

    Run seeds under test

--- 05/src/seeds.cs
+++ 05/src/seeds.cs
@@ -21,43 +21,70 @@ struct SeedRange
 
 class Program
 {
-    static List<SeedRange> seeds = new List<SeedRange>();
-    static List<MapRange> seed_to_soil = new List<MapRange>();
-    static List<MapRange> soil_to_fertilizer = new List<MapRange>();
-    static List<MapRange> fertilizer_to_water = new List<MapRange>();
-    static List<MapRange> water_to_light = new List<MapRange>();
-    static List<MapRange> light_to_temperature = new List<MapRange>();
-    static List<MapRange> temperature_to_humidity = new List<MapRange>();
-    static List<MapRange> humidity_to_location = new List<MapRange>();
-
-    static int Main()
+    static void Main()
     {
         using var input = new StreamReader("input.txt");
 
+        var lookup = new SeedLookup(input);
+
+
+        var min_location = lookup.SearchSmallestLocation();
+
+        // sample result: 35
+        // real result: 261668924
+        Console.WriteLine($"Result: {min_location}");
+    }
+}
+
+public class SeedLookup
+{
+    List<SeedRange> seeds;
+    List<MapRange> seed_to_soil;
+    List<MapRange> soil_to_fertilizer;
+    List<MapRange> fertilizer_to_water;
+    List<MapRange> water_to_light;
+    List<MapRange> light_to_temperature;
+    List<MapRange> temperature_to_humidity;
+    List<MapRange> humidity_to_location;
+
+    public SeedLookup(TextReader input)
+    {
         for (;;)
         {
             string? line = input.ReadLine();
             if (line == null) break;
 
             if (line.StartsWith("seeds: "))
-                ParseSeeds(line.Substring(7));
+                seeds = ParseSeeds(line.Substring(7));
             else if (line.StartsWith("seed-to-soil map:"))
-                ReadMap(input, seed_to_soil);
+                seed_to_soil = ReadMap(input);
             else if (line.StartsWith("soil-to-fertilizer map:"))
-                ReadMap(input, soil_to_fertilizer);
+                soil_to_fertilizer = ReadMap(input);
             else if (line.StartsWith("fertilizer-to-water map:"))
-                ReadMap(input, fertilizer_to_water);
+                fertilizer_to_water = ReadMap(input);
             else if (line.StartsWith("water-to-light map:"))
-                ReadMap(input, water_to_light);
+                water_to_light = ReadMap(input);
             else if (line.StartsWith("light-to-temperature map:"))
-                ReadMap(input, light_to_temperature);
+                light_to_temperature = ReadMap(input);
             else if (line.StartsWith("temperature-to-humidity map:"))
-                ReadMap(input, temperature_to_humidity);
+                temperature_to_humidity = ReadMap(input);
             else if (line.StartsWith("humidity-to-location map:"))
-                ReadMap(input, humidity_to_location);
+                humidity_to_location = ReadMap(input);
         }
 
+        /* to make compiler happy */
+        if (seeds == null) seeds = new();
+        if (seed_to_soil == null) seed_to_soil = new();
+        if (soil_to_fertilizer == null) soil_to_fertilizer = new();
+        if (fertilizer_to_water == null) fertilizer_to_water = new();
+        if (water_to_light == null) water_to_light = new();
+        if (light_to_temperature == null) light_to_temperature = new();
+        if (temperature_to_humidity == null) temperature_to_humidity = new();
+        if (humidity_to_location == null) humidity_to_location = new();
+    }
 
+    public long SearchSmallestLocation()
+    {
         long min_location = long.MaxValue;
 
         foreach (var seed in seeds)
@@ -73,20 +100,17 @@ class Program
             if (location < min_location) min_location = location;
         }
 
-        // sample result: 35
-        // real result: 261668924
-        Console.WriteLine($"Result: {min_location}");
-
-        return 0;
+        return min_location;
     }
 
-    static void ParseSeeds(string seed_str)
+    static List<SeedRange> ParseSeeds(string seed_str)
     {
         var ranges = seed_str
             .Split(" ")
             .Select(long.Parse)
             .ToArray();
 
+        List<SeedRange> seeds = new List<SeedRange>();
         for (int i = 0; i < ranges.Length; i += 1)
         {
             seeds.Add(new SeedRange {
@@ -94,15 +118,19 @@ class Program
                 Size = 1, //ranges[i + 1]
             });
         }
+
+        return seeds;
     }
 
-    static void ReadMap(StreamReader input, List<MapRange> range)
+    static List<MapRange> ReadMap(TextReader input)
     {
+        List<MapRange> range = new List<MapRange>();
+
         int count = 0;
 
         for (;;++count) {
             string? line = input.ReadLine();
-            if (string.IsNullOrEmpty(line)) break;
+            if (string.IsNullOrWhiteSpace(line)) break;
             var values = line.Split(" ").Select(long.Parse).ToList();
 
             range.Add(new MapRange
@@ -112,6 +140,35 @@ class Program
                 Size = values[2],
             });
         }
+
+        return FillGaps(range);
+    }
+
+    static List<MapRange> FillGaps(IEnumerable<MapRange> range)
+    {
+        List<MapRange> target = new List<MapRange>();
+        long prev_end = 0;
+        foreach (var r in range.OrderBy(x => x.Src))
+        {
+            if (r.Src > prev_end)
+                target.Add(new MapRange {
+                    Src = prev_end,
+                    Dest = prev_end,
+                    Size = r.Src - prev_end,
+                });
+            else if (r.Src < prev_end)
+                throw new InvalidOperationException("Range overlap");
+            target.Add(r);
+            prev_end = r.Limit;
+        }
+
+        target.Add(new MapRange {
+            Src = prev_end,
+            Dest = prev_end,
+            Size = long.MaxValue - prev_end,
+        });
+
+        return target;
     }
 
     static long Lookup(long source, List<MapRange> range)
--- 05/tests/SeedsTests.cs
+++ 05/tests/SeedsTests.cs
@@ -2,9 +2,51 @@ namespace seeds.tests;
 
 public class SeedsTests
 {
-    [Fact]
-    public void Test1()
-    {
+    #region SampleInput
+    const string SampleInput = @"
+seeds: 79 14 55 13
 
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
+    ";
+    #endregion
+    [Fact]
+    public void SampleInput_should_produce_expected_Location()
+    {
+        using var input = new StringReader(SampleInput);
+        var sut = new SeedLookup(input);
+
+        var result = sut.SearchSmallestLocation();
+
+        Assert.Equal(35, result);
     }
 }
