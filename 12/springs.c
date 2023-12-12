93a15324eec1f3415f7b241ad6afe5d189dc3cee
Jurģis Brigmanis <velko.x@gmail.com>
2023-12-12 21:40:45 +0200

    Work in progress - new algorithm

--- 12/springs.c
+++ 12/springs.c
@@ -10,13 +10,14 @@
 #define TEMPLATE_LEN    128
 
 static int process_line(char *line);
-static int process_record(const char *template, unsigned *groups);
+static int process_record(const char template[], int start_idx, unsigned groups[]);
 static bool check_counts(char *record, unsigned *groups);
 static void self_tests();
 
 int main(void)
 {
     self_tests();
+    //return 0;
 
     char line[BUFFER_SIZE];
     FILE *input = fopen("input.txt", "r");
@@ -58,21 +59,106 @@ static int process_line(char *line)
     }
     groups[num_groups] = 0;
 
-    int result = process_record(template, groups);
+    int result = process_record(template, 0, groups);
 
     printf("%d\n", result);
 
     char *expected_p1_token = strtok_r(NULL, " \n", &outersp);
     if (expected_p1_token)
     {
-        assert(atoi(expected_p1_token) == result);
+        int expected = atoi(expected_p1_token);
+        assert(expected == result);
     }
 
     //printf("Unk: %d, groups: %d tlen: %lu\n", num_unknown, num_groups, strlen(template));
     return result;
 }
 
-static int process_record(const char *template, unsigned *groups)
+static bool fill_group(char *dest, const char *template, int grp);
+
+static int process_record(const char template[], int start_idx, unsigned groups[])
+{
+    char reconstructed[TEMPLATE_LEN];
+
+    unsigned grp = *groups;
+
+    if (grp == 0)
+        return 1;
+
+    int sub_counts = 0;
+
+    for (;;)
+    {
+        // scan forward until #, ? or \0
+        for( ; template[start_idx] == '.'; ++start_idx);
+
+        switch (template[start_idx])
+        {
+        case '#':
+            /* arrived at beginning of known damage, must try to use all group */
+            strcpy(reconstructed, template);
+            if (fill_group(reconstructed + start_idx, template + start_idx, grp))
+            {
+                // good, go to next group
+                int spacer = 0;
+                if (template[start_idx + grp] == '?')
+                {
+                    reconstructed[start_idx + grp] = '.';
+                    spacer = 1;
+                }
+                return process_record(reconstructed, start_idx + grp + spacer, groups + 1);
+            }
+            else
+                return 0; // did not fit, stop trying
+            break;
+        case '?':
+            /* arrived at the beginning of unknown, can try one by one */
+            strcpy(reconstructed, template);
+            for (; template[start_idx] == '?' || template[start_idx] == '#' ; ++start_idx)
+            {
+                strcpy(reconstructed + start_idx, template + start_idx);
+                if (fill_group(reconstructed + start_idx, template + start_idx, grp))
+                {
+                    // good, try to go deeper
+                    int spacer = 0;
+                    if (template[start_idx + grp] == '?')
+                    {
+                        reconstructed[start_idx + grp] = '.';
+                        spacer = 1;
+                    }
+
+                    sub_counts += process_record(reconstructed, start_idx + grp + spacer, groups + 1);
+                }
+                if (template[start_idx] == '?')
+                    reconstructed[start_idx] = '.';
+            }
+            break;
+        case 0:
+            return sub_counts;
+        default:
+            assert("Unexpected char");
+            break;
+        }
+    }
+    assert("What?");
+    return -1;
+}
+
+static bool fill_group(char *dest, const char *template, int grp)
+{
+    int p;
+    for (p = 0; p < grp; ++p)
+    {
+        if (template[p] == '#' || template[p] == '?')
+            dest[p] = '#';
+        else
+            /* did not fit */
+            return false;
+    }
+    return template[p] != '#';
+}
+
+static int process_record_x(const char *template, unsigned *groups)
 {
     char reconstructed[TEMPLATE_LEN];
     int num_unknown = 0;
@@ -144,4 +230,45 @@ static void self_tests()
     actual = check_counts(record, counts);
     assert(actual == false);
 
+    unsigned c1[] =  { 3, 0 };
+    // not fit
+    int num = process_record("..#?", 0, c1);
+    assert(num == 0);
+
+    // fit exactly
+    num = process_record("..##?", 0, c1);
+    assert(num == 1);
+
+    // fit with trailing good
+    num = process_record("..##?.", 0, c1);
+    assert(num == 1);
+
+    // fit and unknown reminder
+    num = process_record("..##??", 0, c1);
+    assert(num == 1);
+
+    // unknown start trailing #, 1 option
+    num = process_record("..??##", 0, c1);
+    assert(num == 1);
+
+    // can't fit, result too long
+    num = process_record("..#??#", 0, c1);
+    assert(num == 0);
+
+    // need separator
+    unsigned c2[] =  { 1, 1, 0 };
+    num = process_record("???", 0, c2);
+    assert(num == 1);
+
+    // alternating
+    // .#.###.#.######
+    unsigned c3[] =  { 1, 3, 1, 6, 0 };
+    num = process_record("?#?#?#?#?#?#?#?", 0, c3);
+    assert(num == 1);
+
+    // ????.######..#####. 1,6,5
+    // ????.######..#####.
+    unsigned c4[]= {1,6,5,0};
+    num = process_record("????.######..#####.", 0, c4);
+    assert(num == 4);
 }
