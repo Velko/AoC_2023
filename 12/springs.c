0c74f1195f11aca816f1e178261d088de35f9ae8
Jurģis Brigmanis <velko.x@gmail.com>
2023-12-12 11:30:12 +0200

    Implemented, wrong answer

--- 12/springs.c
+++ 12/springs.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
 
 
 #define BUFFER_SIZE     256
+#define MAX_GROUPS        8
+#define TEMPLATE_LEN     25
 
-static int parse_line(const char *line);
+static int process_line(char *line);
+static int process_record(const char *template, unsigned *groups, int num_groups);
+static bool check_counts(char *record, unsigned *groups, int num_groups);
+static void self_tests();
 
 int main(void)
 {
+    //self_tests();
+
     char line[BUFFER_SIZE];
     FILE *input = fopen("input.txt", "r");
 
@@ -14,8 +25,10 @@ int main(void)
 
     for (;;)
     {
-        if (fgets(line, BUFFER_SIZE, input) == NULL) break; 
-        total += parse_line(line);
+        if (fgets(line, BUFFER_SIZE, input) == NULL) break;
+        int unk = process_line(line);
+        if (unk > total)
+            total = unk;
     }
 
     printf("Result: %d\n", total);
@@ -25,7 +38,96 @@ int main(void)
 }
 
 
-static int parse_line(const char *line)
+static int process_line(char *line)
 {
-    return 0;
+    char *outersp, *innersp;
+    char *template = strtok_r(line, " ", &outersp);
+
+    char *groups_token = strtok_r(NULL, " ", &outersp);
+
+    unsigned groups[MAX_GROUPS];
+    int num_groups = 0;
+
+    char *token = strtok_r(groups_token, ",", &innersp);
+    while (token) {
+        groups[num_groups++] = atoi(token);
+        token = strtok_r(NULL, ",", &innersp);
+    }
+
+    //printf("Unk: %d, groups: %d tlen: %lu\n", num_unknown, num_groups, strlen(template));
+    return process_record(template, groups, num_groups);
+}
+
+static int process_record(const char *template, unsigned *groups, int num_groups)
+{
+    char reconstructed[TEMPLATE_LEN];
+    int num_unknown = 0;
+    for (const char *s = template; *s; ++s)
+        num_unknown += *s == '?';
+
+    int possibilites = 0;
+
+    int unk_limit = 1 << num_unknown;
+
+    for (int unk = 0; unk < unk_limit; ++unk)
+    {
+        int i;
+        int unk_bit_idx = 0;
+        for (i = 0; template[i]; ++i)
+        {
+            if (template[i] == '?')
+            {
+                reconstructed[i] = (unk & (1 << unk_bit_idx)) ? '#' : '.';
+                ++unk_bit_idx;
+            }
+            else
+                reconstructed[i] = template[i];
+        }
+        reconstructed[i] = 0;
+
+        if (check_counts(reconstructed, groups, num_groups))
+            ++possibilites;
+    }
+
+    return possibilites;
+}
+
+static bool check_counts(char *record, unsigned *groups, int num_groups)
+{
+    char *savep;
+    char *tok = strtok_r(record, ".", &savep);
+    int c_group;
+    for (c_group = 0; tok && c_group < num_groups; ++c_group )
+    {
+        if (groups[c_group] != strlen(tok))
+            return false;
+
+        tok = strtok_r(NULL, ".", &savep);
+    }
+
+    return tok == NULL && c_group == num_groups;
+}
+
+static void self_tests()
+{
+    char record[20];
+    unsigned counts[]  = { 2, 1 };
+
+    strcpy(record, "..##.#..");
+    bool actual = check_counts(record, counts, 2);
+    assert(actual == true);
+
+    strcpy(record, "##...#");
+    actual = check_counts(record, counts, 2);
+    assert(actual == true);
+
+    strcpy(record, "##...#.#");
+    actual = check_counts(record, counts, 2);
+    assert(actual == false);
+
+
+    strcpy(record, "..##");
+    actual = check_counts(record, counts, 2);
+    assert(actual == false);
+
 }
