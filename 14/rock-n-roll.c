4ee790ace0a4481229eed9266326da53602ebf84
Jurģis Brigmanis <velko.x@gmail.com>
2023-12-14 09:15:50 +0200

    Load and roll

--- 14/rock-n-roll.c
+++ 14/rock-n-roll.c
@@ -1,31 +1,55 @@
 #include <stdio.h>
 
 
-#define BUFFER_SIZE     256
+#define BUFFER_SIZE     120
 
-static int parse_line(const char *line);
+char platform[BUFFER_SIZE][BUFFER_SIZE];
+
+static void roll_all(int nrows);
+static void roll_single(int row, int col);
 
 int main(void)
 {
-    char line[BUFFER_SIZE];
     FILE *input = fopen("input.txt", "r");
+    int nrows = 0;
+    for (;;++nrows)
+    {
+        if (fgets(platform[nrows], BUFFER_SIZE, input) == NULL) break;
+    }
+    fclose(input);
+
+
+    roll_all(nrows);
+
+
+    for (int row = 0; row < nrows; ++row)
+        printf("%s", platform[row]);
 
     int total = 0;
+    printf("Result: %d\n", nrows);
 
-    for (;;)
-    {
-        if (fgets(line, BUFFER_SIZE, input) == NULL) break; 
-        total += parse_line(line);
-    }
-
-    printf("Result: %d\n", total);
-
-    fclose(input);
     return 0;
 }
 
 
-static int parse_line(const char *line)
+static void roll_all(int nrows)
 {
-    return 0;
+    for (int r = 1; r < nrows; ++r)  // no point trying to roll row 0
+    {
+        for (int c = 0; platform[r][c]; ++c)
+        {
+            if (platform[r][c] == 'O')
+                roll_single(r, c);
+        }
+    }
+}
+
+static void roll_single(int row, int col)
+{
+    while (row > 0 && platform[row-1][col] == '.')
+    {
+        platform[row-1][col] = 'O';
+        platform[row][col] = '.';
+        --row;
+    }
 }
