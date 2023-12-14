2296e79a6aded0cae98e9e16b73764b8f02df499
Jurģis Brigmanis <velko.x@gmail.com>
2023-12-14 10:43:33 +0200

    Test with up to 3 cycles

--- 14/rock-n-roll.c
+++ 14/rock-n-roll.c
@@ -9,6 +9,9 @@ int nrows;
 int ncols;
 
 static void roll_all_north();
+static void roll_all_west();
+static void roll_all_south();
+static void roll_all_east();
 static int count_load();
 
 int main(void)
@@ -23,11 +26,16 @@ int main(void)
 
     ncols = strlen(platform[0]) - 1; // discard \0
 
-    roll_all_north();
+    for (int cycles = 0; cycles < 3; ++cycles)
+    {
+        roll_all_north();
+        roll_all_west();
+        roll_all_south();
+        roll_all_east();
+    }
 
-
-    // for (int row = 0; row < nrows; ++row)
-    //      printf("%s", platform[row]);
+    for (int row = 0; row < nrows; ++row)
+         printf("%s", platform[row]);
 
     int load = count_load();
 
@@ -55,6 +63,63 @@ static void roll_all_north()
         }
     }
 }
+
+static void roll_all_west()
+{
+    for (int r = 0; r < nrows; ++r)
+    {
+        for (int c = 1; c < ncols; ++c)  // no point trying to roll col 0
+        {
+            if (platform[r][c] == 'O')
+            {
+                for (int roll_c = c; roll_c > 0 && platform[r][roll_c-1] == '.'; --roll_c)
+                {
+                    platform[r][roll_c-1] = 'O';
+                    platform[r][roll_c] = '.';
+                }
+            }
+        }
+    }
+}
+
+static void roll_all_south()
+{
+    for (int c = 0; c < ncols; ++c)
+    {
+        for (int r = nrows - 2; r >= 0; --r)  // no point trying to roll last row
+        {
+            if (platform[r][c] == 'O')
+            {
+                for (int roll_r = r; roll_r < nrows && platform[roll_r+1][c] == '.'; ++roll_r)
+                {
+                    platform[roll_r+1][c] = 'O';
+                    platform[roll_r][c] = '.';
+                }
+            }
+        }
+    }
+}
+
+
+static void roll_all_east()
+{
+    for (int r = 0; r < nrows; ++r)
+    {
+        for (int c = ncols - 2; c >= 0; --c)  // no point trying to roll last col
+        {
+            if (platform[r][c] == 'O')
+            {
+                for (int roll_c = c; roll_c < ncols && platform[r][roll_c+1] == '.'; ++roll_c)
+                {
+                    platform[r][roll_c+1] = 'O';
+                    platform[r][roll_c] = '.';
+                }
+            }
+        }
+    }
+}
+
+
 
 
 static int count_load()
