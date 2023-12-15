8fff3da7f459c8359d3de838f9dc13198907cbeb
Jurģis Brigmanis <velko.x@gmail.com>
2023-12-15 09:11:40 +0200

    Start of Day 15

---
+++ 15/lens.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+#define BUFFER_SIZE     256
+
+static int parse_line(const char *line);
+
+int main(void)
+{
+    char line[BUFFER_SIZE];
+    FILE *input = fopen("input.txt", "r");
+
+    int total = 0;
+
+    for (;;)
+    {
+        if (fgets(line, BUFFER_SIZE, input) == NULL) break;
+        total += parse_line(line);
+    }
+
+    printf("Result: %d\n", total);
+
+    fclose(input);
+    return 0;
+}
+
+
+static int parse_line(const char *line)
+{
+    return 0;
+}
