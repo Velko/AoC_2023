d5425be31c6594e68f3a5200e8c59184abab5517
Jurģis Brigmanis <velko.x@gmail.com>
2023-12-15 10:15:43 +0200

    Parsing input p2

--- 15/lens.c
+++ 15/lens.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define BUFFER_SIZE     24 * 1024       // the input is one huge string
 
 char line[BUFFER_SIZE];
 
+struct lens
+{
+    char label[8];
+    int focal;
+};
+
 static int calc_hash(const char *str);
+static char parse_lens(char *str, struct lens *lens);
 
 int main(void)
 {
@@ -19,7 +27,13 @@ int main(void)
     char *token = strtok_r(line, ",\n", &savep);
     while (token)
     {
-        total += calc_hash(token);
+        struct lens lens;
+        char op = parse_lens(token, &lens);
+        int box = calc_hash(lens.label);
+
+        printf("%c [%d] %s %d\n", op, box, lens.label, lens.focal);
+
+        //total += calc_hash(token);
 
         token = strtok_r(NULL, ",\n", &savep);
     }
@@ -41,7 +55,31 @@ static int calc_hash(const char *str)
     {
         hash = (hash + *c) * 17;
     }
-    printf("%s %d\n", str, hash);
+    //printf("%s %d\n", str, hash);
 
     return hash;
+}
+
+static char parse_lens(char *str, struct lens *lens)
+{
+    char *minus = strchr(str, '-');
+
+    if (minus)
+    {
+        *minus = 0;
+        if (strlen(str) > 7) { printf("too big\n"); exit(1); }
+        strcpy(lens->label, str);
+        lens->focal = 0;
+        return '-';
+    }
+
+    char *savep;
+    char *label = strtok_r(str, "=", &savep);
+    if (strlen(label) > 7) { printf("too big\n"); exit(1); }
+    strcpy(lens->label, label);
+
+    char *focal = strtok_r(NULL, "=", &savep);
+    lens->focal = atoi(focal);
+
+    return '=';
 }
