67ff63bedcabc881f9edccaba3afd38d575fab60
Jurģis Brigmanis <velko.x@gmail.com>
2023-12-16 12:44:17 +0200

    Single history, count energized, solved

--- 16/floor.c
+++ 16/floor.c
@@ -6,6 +6,7 @@
 #define BUFFER_SIZE     120
 
 char grid[BUFFER_SIZE][BUFFER_SIZE];
+char energized[BUFFER_SIZE][BUFFER_SIZE];
 int nrows;
 int ncols;
 
@@ -27,16 +28,19 @@ struct hist_item
     enum direction dir;
 };
 
-#define HISTORY_MAX     100
-
+#define HISTORY_MAX     (BUFFER_SIZE * BUFFER_SIZE * 4)  // each cell entered from 4 directions
+struct hist_item history[HISTORY_MAX];
+int nhist;
 
 static int move_row(int row, enum direction dir);
 static int move_col(int col, enum direction dir);
-static void walk_path(int start_row, int start_col, enum direction start_dir, struct hist_item *history, int nhist);
+static void walk_path(int start_row, int start_col, enum direction start_dir);
 static void turn_and_split(enum direction start_dir, char tile, enum direction *out_dir1, enum direction *out_dir2);
 
 int main(void)
 {
+    memset(energized, '.', sizeof(energized));
+
     FILE *input = fopen("input.txt", "r");
 
     nrows = 0;
@@ -46,30 +50,39 @@ int main(void)
         if (ncols == 0)
             ncols = strlen(grid[nrows]) - 1;
         grid[nrows][ncols] = 0;
+        energized[nrows][ncols] = 0;
     }
+    fclose(input);
 
+    nhist = 0;
 
-    struct hist_item history[HISTORY_MAX];
+    walk_path(0, 0, RIGHT);
 
-    walk_path(0, 0, RIGHT, history, 0);
-
-    //for (int r = 0; r < nrows; ++r)
-    //    printf("'%s'\n", grid[r]);
+    // for (int r = 0; r < nrows; ++r)
+    //    printf("%s\n", energized[r]);
 
     int total = 0;
+    for (int r = 0; r < nrows; ++r)
+    {
+        for (int c = 0; c < ncols; ++c)
+        {
+            total += energized[r][c] == '#';
+        }
+    }
+
+    // result p1: 7517
     printf("Result: %d\n", total);
 
-    fclose(input);
     return 0;
 }
 
 
-static void walk_path(int start_row, int start_col, enum direction start_dir, struct hist_item *history, int nhist)
+static void walk_path(int start_row, int start_col, enum direction start_dir)
 {
-    printf("(%d, %d) ", start_row, start_col);
+    //printf("(%d, %d) ", start_row, start_col);
     if (start_row < 0 || start_row >= nrows || start_col < 0 || start_col >= ncols)
     {
-        printf("X\n");
+        //printf("X\n");
         return;
     }
 
@@ -77,7 +90,7 @@ static void walk_path(int start_row, int start_col, enum direction start_dir, st
     {
         if (history[h].row == start_row && history[h].col == start_col && history[h].dir == start_dir)
         {
-            printf("O\n");
+            //printf("O\n");
             return;
         }
     }
@@ -88,17 +101,19 @@ static void walk_path(int start_row, int start_col, enum direction start_dir, st
     history[nhist].dir = start_dir;
     ++nhist;
 
+    energized[start_row][start_col] = '#';
+
     enum direction dir1;
     enum direction dir2;
     turn_and_split(start_dir, grid[start_row][start_col], &dir1, &dir2);
     {
-        printf("%c (1)\n", dir_str[dir1]);
+        //printf("%c (1)\n", dir_str[dir1]);
 
         int row1 = move_row(start_row, dir1);
         int col1 = move_col(start_col, dir1);
 
 
-        walk_path(row1, col1, dir1, history, nhist);
+        walk_path(row1, col1, dir1);
     }
 
     if (dir1 != dir2) // split
@@ -106,12 +121,9 @@ static void walk_path(int start_row, int start_col, enum direction start_dir, st
         int row2 = move_row(start_row, dir2);
         int col2 = move_col(start_col, dir2);
 
-        printf("(%d, %d) %c (2)\n", start_row, start_col, dir_str[dir2]);
+        //printf("(%d, %d) %c (2)\n", start_row, start_col, dir_str[dir2]);
 
-        struct hist_item split_history[HISTORY_MAX];
-        memcpy(split_history, history, nhist * sizeof(struct hist_item));
-
-        walk_path(row2, col2, dir2, split_history, nhist);
+        walk_path(row2, col2, dir2);
     }
 }
 
