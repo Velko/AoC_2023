342e5c1051f80ff78aab78aa34e411a2a89490bb
Jurģis Brigmanis <velko.x@gmail.com>
2023-12-18 12:31:17 +0200

    Part1 solved

--- 18/lagoon.c
+++ 18/lagoon.c
@@ -17,7 +17,26 @@ struct instr
 struct instr instructions[MAX_INSTRUCTIONS];
 int ninstructions;
 
+
+#define GROUND_SIZE     400
+
+char ground[GROUND_SIZE][GROUND_SIZE];
+int nrows;
+int ncols;
+int startrow;
+int startcol;
+
+#define TEMP_MARKER     '*'
+#define BORDER_MARKER   '#'
+#define EMPTY_MARKER    '.'
+
 static void parse_line(const char *line);
+static void measure();
+static void prepare_ground();
+static void print_ground();
+static void mark_border();
+static void flood_outside();
+static int count_border_and_inside();
 
 int main(void)
 {
@@ -29,11 +48,20 @@ int main(void)
         if (fgets(line, BUFFER_SIZE, input) == NULL) break; 
         parse_line(line);
     }
-
-    // int total = 0;
-    printf("Result: %d\n", ninstructions);
-
     fclose(input);
+
+
+    measure();
+    prepare_ground();
+    mark_border();
+    flood_outside();
+    print_ground();
+
+
+    int result = count_border_and_inside();
+
+    // result p1: 40131
+    printf("Result: %d\n", result);
     return 0;
 }
 
@@ -42,4 +70,177 @@ static void parse_line(const char *line)
 {
     sscanf(line, "%c %d", &instructions[ninstructions].dir, &instructions[ninstructions].steps);
     //printf("%c, %d\n", instructions[ninstructions].dir, instructions[ninstructions].steps);
+}
+
+static void measure()
+{
+    int row_min = 100000;
+    int row_max = -100000;
+
+    int col_min = 100000;
+    int col_max = -100000;
+
+    int row = 0;
+    int col = 0;
+    for (int i = 0; i < ninstructions; ++i)
+    {
+        switch (instructions[i].dir)
+        {
+        case 'D':
+            row += instructions[i].steps;
+            if (row > row_max) row_max = row;
+            break;
+        case 'U':
+            row -= instructions[i].steps;
+            if (row < row_min) row_min = row;
+            break;
+        case 'R':
+            col += instructions[i].steps;
+            if (col > col_max) col_max = col;
+            break;
+        case 'L':
+            col -= instructions[i].steps;
+            if (col < col_min) col_min = col;
+            break;
+        default:
+            printf("WTF\n");
+            exit(1);
+            break;
+        }
+    }
+
+    // add 1-char border around
+    nrows = row_max - row_min + 1 + 2;
+    ncols = col_max - col_min + 1 + 2;
+    startrow = -row_min + 1;
+    startcol = -col_min + 1;
+
+    printf("(%d, %d) to (%d, %d) (%dx%d)\n", row_min, col_min, row_max, col_max, nrows, ncols);
+}
+
+static void mark_border()
+{
+    int row = startrow;
+    int col = startcol;
+
+    ground[row][col] = BORDER_MARKER;
+
+    for (int i = 0; i < ninstructions; ++i)
+    {
+        switch (instructions[i].dir)
+        {
+        case 'D':
+            for (int s = 0; s < instructions[i].steps; ++s)
+            {
+                ++row;
+                ground[row][col] = BORDER_MARKER;
+            }
+            break;
+        case 'U':
+            for (int s = 0; s < instructions[i].steps; ++s)
+            {
+                --row;
+                ground[row][col] = BORDER_MARKER;
+            }
+            break;
+        case 'R':
+            for (int s = 0; s < instructions[i].steps; ++s)
+            {
+                ++col;
+                ground[row][col] = BORDER_MARKER;
+            }
+            break;
+        case 'L':
+            for (int s = 0; s < instructions[i].steps; ++s)
+            {
+                --col;
+                ground[row][col] = BORDER_MARKER;
+            }
+            break;
+        default:
+            printf("WTF\n");
+            exit(1);
+            break;
+        }
+    }
+}
+
+static void prepare_ground()
+{
+    memset(ground, TEMP_MARKER, sizeof(ground));
+    for (int row = 0; row < nrows; ++row)
+        ground[row][ncols] = 0;
+}
+
+static void print_ground()
+{
+    for (int row = 0; row < nrows; ++row)
+        printf("%s\n", ground[row]);
+}
+
+static int count_border_and_inside()
+{
+    int count = 0;
+    for (int row = 0; row < nrows; ++row)
+    {
+        for (int col = 0; col < ncols; ++col)
+        {
+            if (ground[row][col] == BORDER_MARKER || ground[row][col] == TEMP_MARKER)
+                ++count;
+        }
+    }
+
+    return count;
+}
+
+
+static bool mark_around(int row, int col);
+static void flood_outside()
+{
+    bool marked;
+    ground[0][0] = EMPTY_MARKER;
+
+    do
+    {
+        marked = false;
+        for (int row = 0; row < nrows; ++row)
+        {
+            for (int col = 0; col < ncols; ++col)
+            {
+                if (ground[row][col] == EMPTY_MARKER)
+                    marked |= mark_around(row, col);
+            }
+        }
+    } while (marked);
+}
+
+static bool mark_around(int row, int col)
+{
+    bool marked = false;
+
+    if (row > 0 && ground[row - 1][col] == TEMP_MARKER)
+    {
+        ground[row - 1][col] = EMPTY_MARKER;
+        marked = true;
+    }
+
+    if (col > 0 && ground[row][col - 1] == TEMP_MARKER)
+    {
+        ground[row][col - 1] = EMPTY_MARKER;
+        marked = true;
+    }
+
+    if (ground[row + 1][col] == TEMP_MARKER)
+    {
+        ground[row + 1][col] = EMPTY_MARKER;
+        marked = true;
+    }
+
+    if (ground[row][col + 1] == TEMP_MARKER)
+    {
+        ground[row][col + 1] = EMPTY_MARKER;
+        marked = true;
+    }
+
+    return marked;
 }
