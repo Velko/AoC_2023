5e7ab3b26a1d630373fb76c3cdb7342ad74c6215
Jurģis Brigmanis <velko.x@gmail.com>
2023-12-22 21:51:40 +0200

    Object-orientize and cleanup

--- 22/jenga.c
+++ 22/jenga.c
@@ -22,58 +22,54 @@ struct cube
 
 #define MAX_CUBES       1500
 
-struct cube cubes[MAX_CUBES];
-int ncubes;
-
-
-
 #define MAX_Z       350
 #define MAX_Y        10
 #define MAX_X        10
 
-bool pile[MAX_Z][MAX_Y][MAX_X];
+struct state
+{
+    struct cube cubes[MAX_CUBES];
+    int ncubes;
+    bool pile[MAX_Z][MAX_Y][MAX_X];
+};
 
+struct state root_state;
 
+static void init_state(struct state *state);
+static void parse_line(struct state *state, const char *line);
+static bool is_place_occupied(struct state *state, struct cube *cube);
+static bool fall_cube(struct state *state, int cube_idx);
+static void sort_cubes(struct state *state);
+static void drop_all_cubes(struct state *state);
+static int num_will_fall_if_removed(struct state *state, int cube_idx);
 
-static void parse_line(const char *line);
-static bool is_place_occupied(struct cube *cube);
-static bool fall_cube(int cube_idx);
-static void sort_cubes();
-static void measure();
-static int num_will_fall_if_removed(int cube_idx);
 int main(void)
 {
     char line[BUFFER_SIZE];
     FILE *input = fopen("input.txt", "r");
 
-    ncubes = 0;
+    init_state(&root_state);
     for (;;)
     {
         if (fgets(line, BUFFER_SIZE, input) == NULL) break; 
-        parse_line(line);
+        parse_line(&root_state, line);
     }
     fclose(input);
 
-    measure();
+    sort_cubes(&root_state);
 
-    sort_cubes();
-
-    // initial fall
-    for (int c = 0; c < ncubes; ++c)
-    {
-        fall_cube(c);
-    }
+    drop_all_cubes(&root_state);
 
     int result1 = 0;
     int result2 = 0;
 
-    for (int c = 0; c < ncubes; ++c)
+    for (int c = 0; c < root_state.ncubes; ++c)
     {
-        int fell = num_will_fall_if_removed(c);
+        int fell = num_will_fall_if_removed(&root_state, c);
+
         if (fell == 0)
-        {
             ++result1;
-        }
+
         result2 += fell;
     }
 
@@ -88,37 +84,50 @@ int main(void)
 }
 
 
-static void parse_line(const char *line)
+static void init_state(struct state *state)
 {
+    memset(state, 0, sizeof(struct state));
+}
+
+static void parse_line(struct state *state, const char *line)
+{
+    struct cube *cube = &state->cubes[state->ncubes];
+
     int n = sscanf(line, "%d,%d,%d~%d,%d,%d",
-        &cubes[ncubes].start.x,
-        &cubes[ncubes].start.y,
-        &cubes[ncubes].start.z,
-        &cubes[ncubes].end.x,
-        &cubes[ncubes].end.y,
-        &cubes[ncubes].end.z
+        &cube->start.x,
+        &cube->start.y,
+        &cube->start.z,
+        &cube->end.x,
+        &cube->end.y,
+        &cube->end.z
     );
     assert(n == 6);
 
     // validate that *start* is always lower
-    assert(cubes[ncubes].start.z <= cubes[ncubes].end.z);
-    ++ncubes;
+    assert(cube->start.z <= cube->end.z);
+    ++state->ncubes;
 }
 
-static bool can_fall(int cube_idx)
+static void drop_all_cubes(struct state *state)
 {
-    if (cubes[cube_idx].start.z == 1) return false;
+    for (int c = 0; c < state->ncubes; ++c)
+        fall_cube(state, c);
+}
 
-    struct cube candidate = cubes[cube_idx];
+static bool can_fall(struct state *state, int cube_idx)
+{
+    if (state->cubes[cube_idx].start.z == 1) return false;
+
+    struct cube candidate = state->cubes[cube_idx];
     --candidate.start.z;
     --candidate.end.z;
 
-    return !is_place_occupied(&candidate);
+    return !is_place_occupied(state, &candidate);
 }
 
-static void set_pile(int cube_idx, bool occupied)
+static void set_pile(struct state *state, int cube_idx, bool occupied)
 {
-    struct cube *cube = &cubes[cube_idx];
+    struct cube *cube = &state->cubes[cube_idx];
 
     assert(cube->start.z <= cube->end.z);
     assert(cube->end.z < MAX_Z);
@@ -132,56 +141,51 @@ static void set_pile(int cube_idx, bool occupied)
             assert(cube->end.x < MAX_X);
             for (int x = cube->start.x; x <= cube->end.x; ++x)
             {
-                pile[z][y][x] = occupied;
+                state->pile[z][y][x] = occupied;
             }
         }
     }
 }
 
-static bool fall_cube(int cube_idx)
+static bool fall_cube(struct state *state, int cube_idx)
 {
     bool fell = false;
-    set_pile(cube_idx, false);
+    set_pile(state, cube_idx, false);
 
-    while (can_fall(cube_idx))
+    while (can_fall(state, cube_idx))
     {
-        --cubes[cube_idx].start.z;
-        --cubes[cube_idx].end.z;
+        --state->cubes[cube_idx].start.z;
+        --state->cubes[cube_idx].end.z;
         fell = true;
     }
 
     // cube has fallen as low as it will go, settle it in
-    set_pile(cube_idx, true);
+    set_pile(state, cube_idx, true);
 
     return fell;
 }
 
-static int num_will_fall_if_removed(int cube_idx)
+static int num_will_fall_if_removed(struct state *state, int cube_idx)
 {
-    bool backup_pile[MAX_Z][MAX_Y][MAX_X];
-    struct cube backup_cubes[MAX_CUBES];
+    struct state copy;
+    memcpy(&copy, state, sizeof(struct state));
 
-    memcpy(backup_pile, pile, sizeof(pile));
-    memcpy(backup_cubes, cubes, sizeof(cubes));
-
-    set_pile(cube_idx, false);
+    set_pile(&copy, cube_idx, false);
 
     int will_fall = 0;
 
-    for (int c = 0; c < ncubes; ++c)
+    for (int c = 0; c < copy.ncubes; ++c)
     {
-        if (c != cube_idx)
-            if (fall_cube(c))
-                ++will_fall;
-    }
+        if (c == cube_idx) continue;
 
-    memcpy(pile, backup_pile, sizeof(pile));
-    memcpy(cubes, backup_cubes, sizeof(cubes));
+        if (fall_cube(&copy, c))
+            ++will_fall;
+    }
 
     return will_fall;
 }
 
-static bool is_place_occupied(struct cube *cube)
+static bool is_place_occupied(struct state *state, struct cube *cube)
 {
     // probably can do smarter check but this should do
     assert(cube->start.y <= cube->end.y);
@@ -190,7 +194,7 @@ static bool is_place_occupied(struct cube *cube)
         assert(cube->start.x <= cube->end.x);
         for (int x = cube->start.x; x <= cube->end.x; ++x)
         {
-            if (pile[cube->start.z][y][x])
+            if (state->pile[cube->start.z][y][x])
                 return true;
         }
     }
@@ -199,43 +203,12 @@ static bool is_place_occupied(struct cube *cube)
 }
 
 static int cmp_cubes_z(const void *a, const void *b);
-static void sort_cubes()
+static void sort_cubes(struct state *state)
 {
-    qsort(cubes, ncubes, sizeof(struct cube), cmp_cubes_z);
+    qsort(state->cubes, state->ncubes, sizeof(struct cube), cmp_cubes_z);
 }
 
 static int cmp_cubes_z(const void *a, const void *b)
 {
     return ((const struct cube *)a)->start.z - ((const struct cube *)b)->start.z;
-}
-
-static void measure()
-{
-    struct coord min_c = { .x = INT_MAX, .y = INT_MAX, .z = INT_MAX };
-    struct coord max_c = { .x = INT_MIN, .y = INT_MIN, .z = INT_MIN };
-
-    for (int c = 0; c < ncubes; ++c)
-    {
-        if (max_c.x < cubes[c].start.x) max_c.x = cubes[c].start.x;
-        if (max_c.y < cubes[c].start.y) max_c.y = cubes[c].start.y;
-        if (max_c.z < cubes[c].start.z) max_c.z = cubes[c].start.z;
-        if (min_c.x > cubes[c].start.x) min_c.x = cubes[c].start.x;
-        if (min_c.y > cubes[c].start.y) min_c.y = cubes[c].start.y;
-        if (min_c.z > cubes[c].start.z) min_c.z = cubes[c].start.z;
-        if (max_c.x < cubes[c].end.x) max_c.x = cubes[c].end.x;
-        if (max_c.y < cubes[c].end.y) max_c.y = cubes[c].end.y;
-        if (max_c.z < cubes[c].end.z) max_c.z = cubes[c].end.z;
-        if (min_c.x > cubes[c].end.x) min_c.x = cubes[c].end.x;
-        if (min_c.y > cubes[c].end.y) min_c.y = cubes[c].end.y;
-        if (min_c.z > cubes[c].end.z) min_c.z = cubes[c].end.z;
-    }
-
-    printf("(%d, %d, %d) - (%d, %d, %d)\n",
-        min_c.x,
-        min_c.y,
-        min_c.z,
-        max_c.x,
-        max_c.y,
-        max_c.z
-    );
 }
