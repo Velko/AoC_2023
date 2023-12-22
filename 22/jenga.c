5eea542de604af2d5b64e4dfb012c31e56032a4d
Jurģis Brigmanis <velko.x@gmail.com>
2023-12-22 10:17:40 +0200

    Parse input

--- 22/jenga.c
+++ 22/jenga.c
@@ -6,29 +6,71 @@
 
 #define BUFFER_SIZE     256
 
-static int parse_line(const char *line);
+struct coord
+{
+    int x;
+    int y;
+    int z;
+};
+
+struct cube
+{
+    struct coord start;
+    struct coord end;
+};
+
+#define MAX_CUBES       1500
+
+struct cube cubes[MAX_CUBES];
+int ncubes;
+
+
+static void parse_line(const char *line);
 
 int main(void)
 {
     char line[BUFFER_SIZE];
     FILE *input = fopen("input.txt", "r");
 
-    int total = 0;
-
+    ncubes = 0;
     for (;;)
     {
         if (fgets(line, BUFFER_SIZE, input) == NULL) break; 
-        total += parse_line(line);
+        parse_line(line);
+    }
+    fclose(input);
+
+    for (int c = 0; c < ncubes; ++c)
+    {
+        printf("(%d, %d, %d)\t[%d, %d, %d]\n",
+            cubes[c].start.x,
+            cubes[c].start.y,
+            cubes[c].start.z,
+            cubes[c].end.x - cubes[c].start.x + 1,
+            cubes[c].end.y - cubes[c].start.y + 1,
+            cubes[c].end.z - cubes[c].start.z + 1
+        );
+
     }
 
+    int total = 0;
     printf("Result: %d\n", total);
 
-    fclose(input);
+    
     return 0;
 }
 
 
-static int parse_line(const char *line)
+static void parse_line(const char *line)
 {
-    return 0;
+    int n = sscanf(line, "%d,%d,%d~%d,%d,%d",
+        &cubes[ncubes].start.x,
+        &cubes[ncubes].start.y,
+        &cubes[ncubes].start.z,
+        &cubes[ncubes].end.x,
+        &cubes[ncubes].end.y,
+        &cubes[ncubes].end.z
+    );
+    assert(n == 6);
+    ++ncubes;
 }
