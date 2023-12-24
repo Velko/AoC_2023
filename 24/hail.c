488d277a8ef0a77f6c67a34a48f10428ce99a1cd
Jurģis Brigmanis <velko.x@gmail.com>
2023-12-25 00:41:04 +0200

    Math and first implementation

--- 24/hail.c
+++ 24/hail.c
@@ -6,29 +6,146 @@
 
 #define BUFFER_SIZE     256
 
-static int parse_line(const char *line);
+#define TEST_DATA
+
+#ifdef TEST_DATA
+#define TEST_AREA_MIN         7
+#define TEST_AREA_MAX         27
+#else
+#define TEST_AREA_MIN         200000000000000
+#define TEST_AREA_MAX         400000000000000
+#endif
+
+#define TEST_AREA_X_MIN         TEST_AREA_MIN
+#define TEST_AREA_Y_MIN         TEST_AREA_MIN
+
+#define TEST_AREA_X_MAX         TEST_AREA_MAX
+#define TEST_AREA_Y_MAX         TEST_AREA_MAX
+
+
+struct point3d
+{
+    long x;
+    long y;
+    long z;
+};
+
+struct hail
+{
+    struct point3d position;
+    struct point3d velocity;
+};
+
+#define MAX_HAIL        300
+
+struct hail hailstones[MAX_HAIL];
+int nhailstones;
+
+static void parse_line(const char *line);
+static bool calc_intersect(struct hail *a, struct hail *b);
+static void print_hail(struct hail *a);
 
 int main(void)
 {
     char line[BUFFER_SIZE];
+    #ifdef TEST_DATA
+    FILE *input = fopen("sample_input.txt", "r");
+    #else
     FILE *input = fopen("input.txt", "r");
-
-    int total = 0;
+    #endif
 
     for (;;)
     {
         if (fgets(line, BUFFER_SIZE, input) == NULL) break; 
-        total += parse_line(line);
+        parse_line(line);
+    }
+    fclose(input);
+
+    int total = 0;
+
+    for (int j = 0; j < nhailstones; ++j)
+    {
+        for (int i = j + 1; i < nhailstones; ++i)
+        {
+            if (calc_intersect(hailstones + j, hailstones + i))
+                ++total;
+        }
     }
 
     printf("Result: %d\n", total);
 
-    fclose(input);
+    
     return 0;
 }
 
 
-static int parse_line(const char *line)
+static void parse_line(const char *line)
 {
-    return 0;
+    struct hail *h = hailstones + nhailstones;
+
+    int n = sscanf(line, "%ld, %ld, %ld @ %ld, %ld, %ld",
+        &h->position.x,
+        &h->position.y,
+        &h->position.z,
+        &h->velocity.x,
+        &h->velocity.y,
+        &h->velocity.z);
+    
+    assert(n == 6);
+
+    ++nhailstones;
+}
+
+
+static bool calc_intersect(struct hail *a, struct hail *b)
+{
+    printf("\n");
+    print_hail(a);
+    print_hail(b);
+
+
+    long dividend_b = (b->position.y - a->position.y) * a->velocity.x - (b->position.x - a->position.x) * a->velocity.y;
+    long divisor_b = b->velocity.x * a->velocity.y - b->velocity.y * a->velocity.x;
+
+    long dividend_a = (a->position.y - b->position.y) * b->velocity.x - (a->position.x - b->position.x) * b->velocity.y;
+    long divisor_a = a->velocity.x * b->velocity.y - a->velocity.y * b->velocity.x;
+
+    if (divisor_b == 0) // parallel
+    {
+        printf("Parallel\n");
+        return false;
+    }
+
+    if ((dividend_b > 0 && divisor_b < 0) || (dividend_b < 0 && divisor_b > 0)) // signs differ, intersected in past
+    {
+        printf("Past B\n");
+        return false;
+    }
+
+    if ((dividend_a > 0 && divisor_a < 0) || (dividend_a < 0 && divisor_a > 0)) // signs differ, intersected in past
+    {
+        printf("Past A\n");
+        return false;
+    }
+
+    double intersect_x = b->position.x + (b->velocity.x * dividend_b / (double)divisor_b);
+    double intersect_y = b->position.y + (b->velocity.y * dividend_b / (double)divisor_b);
+
+    bool fits = intersect_x >= TEST_AREA_X_MIN && intersect_x <= TEST_AREA_X_MAX
+        && intersect_y >= TEST_AREA_Y_MIN && intersect_y <= TEST_AREA_Y_MAX;
+
+    printf("Intersects %s at %g, %g\n", fits ? "inside" : "outside", intersect_x, intersect_y);
+
+    return fits;
+}
+
+static void print_hail(struct hail *h)
+{
+    printf("%ld, %ld, %ld @ %ld, %ld, %ld\n",
+        h->position.x,
+        h->position.y,
+        h->position.z,
+        h->velocity.x,
+        h->velocity.y,
+        h->velocity.z);
 }
