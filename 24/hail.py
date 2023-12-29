2f29f065b853eb798c8de3a18482e175df95e5d9
Jurģis Brigmanis <velko.x@gmail.com>
2023-12-30 00:45:08 +0200

    Attempting to solve 24 using Gauss

--- 24/hail.py
+++ 24/hail.py
@@ -68,8 +68,56 @@ def compare_hail(a, b):
 
     return fits
 
+def solve_p2_z3():
+    (xa0, ya0, za0), (vax, vay, vaz) = hailstones[0]
+    (xb0, yb0, zb0), (vbx, vby, vbz) = hailstones[1]
+    (xc0, yc0, zc0), (vcx, vcy, vcz) = hailstones[2]
 
-with open("input.txt") as f:
+    ta, tb, tc, vrx, vry, vrz, xr0, yr0, zr0 = z3.Reals("ta tb tc vrx vry vrz xr0 yr0 zr0")
+
+    equations = [
+        xr0 + vrx * ta == xa0 + vax * ta,
+        yr0 + vry * ta == ya0 + vay * ta,
+        zr0 + vrz * ta == za0 + vaz * ta,
+
+        xr0 + vrx * tb == xb0 + vbx * tb,
+        yr0 + vry * tb == yb0 + vby * tb,
+        zr0 + vrz * tb == zb0 + vbz * tb,
+
+        xr0 + vrx * tc == xc0 + vcx * tc,
+        yr0 + vry * tc == yc0 + vcy * tc,
+        zr0 + vrz * tc == zc0 + vcz * tc,
+    ]
+
+    solver = z3.Solver()
+    solver.add(*equations)
+    solver.check()
+    m = solver.model();
+
+    result2 = m[xr0].as_long() + m[yr0].as_long() + m[zr0].as_long()
+    print (f"Result p2: {result2}")
+
+def solve_p2_gauss():
+    (xa0, ya0, za0), (vax, vay, vaz) = hailstones[0]
+    (xb0, yb0, zb0), (vbx, vby, vbz) = hailstones[1]
+    (xc0, yc0, zc0), (vcx, vcy, vcz) = hailstones[2]
+
+    matrix = [
+        [vby - vay, vax - vbx,         0, ya0 - yb0, xb0 - xa0,         0, xb0 * vby - yb0 * vbx - xa0 * vay + ya0 * vax],
+        [vbz - vaz,         0, vax - vbx, za0 - zb0,         0, xb0 - xa0, xb0 * vbz - zb0 * vbx - xa0 * vaz + za0 * vax],
+        [        0, vbz - vaz, vay - vby,         0, za0 - zb0, yb0 - ya0, yb0 * vbz - zb0 * vby - ya0 * vaz + za0 * vay],
+        [vcy - vay, vax - vcx,         0, ya0 - yc0, xc0 - xa0,         0, xc0 * vcy - yc0 * vcx - xa0 * vay + ya0 * vax],
+        [vcz - vaz,         0, vax - vcx, za0 - zc0,         0, xc0 - xa0, xc0 * vcz - zc0 * vcx - xa0 * vaz + za0 * vax],
+        [        0, vcz - vaz, vay - vcy,         0, za0 - zc0, yc0 - ya0, yc0 * vcz - zc0 * vcy - ya0 * vaz + za0 * vay],
+    ]
+
+    if not gauss_eliminate(matrix):
+        raise Exception("WTF?")
+
+
+
+
+with open("24/input.txt") as f:
     for line in f:
         p, v = line.split("@")
         pos = tuple(map(lambda x: int(x.strip()), p.split(',')))
@@ -85,31 +133,6 @@ for i, a in enumerate(hailstones):
 
 print (f"Result p1: {result1}")
 
+solve_p2_z3()
+solve_p2_gauss()
 
-(xa0, ya0, za0), (vax, vay, vaz) = hailstones[0]
-(xb0, yb0, zb0), (vbx, vby, vbz) = hailstones[1]
-(xc0, yc0, zc0), (vcx, vcy, vcz) = hailstones[2]
-
-ta, tb, tc, vrx, vry, vrz, xr0, yr0, zr0 = z3.Reals("ta tb tc vrx vry vrz xr0 yr0 zr0")
-
-equations = [
-    xr0 + vrx * ta == xa0 + vax * ta,
-    yr0 + vry * ta == ya0 + vay * ta,
-    zr0 + vrz * ta == za0 + vaz * ta,
-
-    xr0 + vrx * tb == xb0 + vbx * tb,
-    yr0 + vry * tb == yb0 + vby * tb,
-    zr0 + vrz * tb == zb0 + vbz * tb,
-
-    xr0 + vrx * tc == xc0 + vcx * tc,
-    yr0 + vry * tc == yc0 + vcy * tc,
-    zr0 + vrz * tc == zc0 + vcz * tc,
-]
-
-solver = z3.Solver()
-solver.add(*equations)
-solver.check()
-m = solver.model();
-
-result2 = m[xr0].as_long() + m[yr0].as_long() + m[zr0].as_long()
-print (f"Result p2: {result2}")
